0c3549b54f958e09443b6d020b0bf16d40e5244e
Adomako08 <b.adomako05@gmail.com>
2024-02-10 16:01:16 -0800

    5-flip_bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,20 +5,20 @@
  * you would need to flip to get from one number to another
  * @n: first number
  * @m: second number
- *
+ * @i: count set bits in i
  * Return: number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+	unsigned long int i = n ^ m;
+	unsigned int count = 0;
 
-	for (i = 63; i >= 0; i--)
+	while (i != 0)
 	{
-		current = exclusive >> 1;
-		if (current & 1)
+		if (i & 1)
 			count++;
+
+		i >>= 1;
 	}
 
 	return (count);
